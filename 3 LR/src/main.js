02750175a5a52656b7ccfe102e48a1ae0014f934
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2024-10-08 20:21:12 +0300

    Add files via upload

---
+++ 3 LR/src/main.js
@@ -0,0 +1,19 @@
+import HeaderComponent from './view/header-component.js'
+import FormComponent from './view/form-component.js';
+import { render, RenderPosition } from '../src/framework/render.js';
+import TasksAreaPresenter from './presenter/task-area-presenter.js';
+import TasksModel from './model/task-model.js';
+
+
+const bodyContainer = document.querySelector('.board-app');
+render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
+
+const formContainer = document.querySelector('.form-task');
+render(new FormComponent(), formContainer);
+
+const tasksAreaContainer = document.querySelector(".task-area");
+
+const tasksModel = new TasksModel();
+const tasksAreaPresenter = new TasksAreaPresenter({ areaContainer: tasksAreaContainer, tasksModel: tasksModel });
+
+ tasksAreaPresenter.init();
