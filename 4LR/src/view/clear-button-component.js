0e9a5e581b29f478b90543061244c2b937f597e4
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2024-10-21 21:18:48 +0300

    Create clear-button-component.js

---
+++ 4LR/src/view/clear-button-component.js
@@ -0,0 +1,14 @@
+import { createElement } from '../framework/render.js';
+import { AbstractComponent } from '../framework/view/abstract-component.js';
+
+function createClearButtonTemplate() {
+  return `
+    <button class="clear-btn" type="button">Очистить корзину</button>
+  `;
+}
+
+export default class ClearButtonComponent extends AbstractComponent {
+  get template() {
+    return createClearButtonTemplate();
+  }
+}
